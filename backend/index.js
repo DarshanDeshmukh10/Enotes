99852591c31c656e1ae64703381b86479a716b91
darshandeshmukh10 <deshmukhdarshan447@gmail.com>
2023-02-14 23:25:40 +0530

    commit2

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const connectToMongo=require('./db');
+const express = require('express')
+connectToMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use('/api/auth',require('./routes/auth'))
+//app.use('/api/auth',require('./routes/notes'))
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
